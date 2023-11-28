bdd5fbd2f8fa702b23e996e199c3cb5ed5b63cde
Taynara Teixeira <42852460+TayTeixeira7@users.noreply.github.com>
2023-11-28 14:12:51 -0300

    tudo finalizado

---
+++ AgradecimentoParticipacao.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Função para redirecionar após 3 segundos
+    setTimeout(function() {
+        window.location.href = './Coleta.html';
+    }, 3000);
+});
